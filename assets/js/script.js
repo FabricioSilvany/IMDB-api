bf252b9e988868b8bb3fc51beca1ae961db6092a
FabricioSilvany <fabriciosilvany57@gmail.com>
2025-05-27 14:40:57 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //dom
 const frmPesquisa = document.querySelector('form')
 const key = '63dddcfc'
-const lista = document.querySelector('div.lista')
+const lista = document.querySelector("div.lista")
 
 //eventos
 frmPesquisa.onsubmit = (ev) => {
@@ -14,12 +14,26 @@ frmPesquisa.onsubmit = (ev) => {
         return
     }
 
-    fetch(`https://www.omdbapi.com/?apikey=${key}&s=${pesquisa}`)
+    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${key}`)
         .then(result => result.json())
-        .then(json => console.carregaLista(json));  
-}
+        .then(json => carregaLista(json));
+}   
 
-    const carregaLista = (json) => {
-    lista.innerHTML = ""
+const carregaLista = (json) => {
+    lista.innerHTML = "";
 
+    if (json.Response == 'False'){
+        alert('Nenhum filme encontrado')
+        return
+    }
+    
+    json.Search.forEach(element => {
+        
+        let item = document.createElement("div")
+        item.classList.add("item");
+
+        item.innerHTML =  `<img src="${element.Poster}"  /><h2>${element.Title}</h2>`;
+
+        lista.appendChild(item);
+    });
 }
